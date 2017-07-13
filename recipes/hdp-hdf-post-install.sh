dfb2b2a5c9e814b9f2266bcd3cbf5c1f51f721bc
geetha-hortonworks <ganne@hortonworks.com>
2017-07-13 15:33:09 -0700

    final hdp-hdf-install script

--- recipes/hdp-hdf-post-install.sh
+++ recipes/hdp-hdf-post-install.sh
@@ -22,6 +22,23 @@ fi
 export HADOOP_USER_NAME=hdfs
 echo "*********************************HADOOP_USER_NAME set to HDFS"
 
+installUtils () {
+	echo "*********************************Installing WGET..."
+	yum install -y wget
+
+	echo "*********************************Installing Maven..."
+	wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O 	/etc/yum.repos.d/epel-apache-maven.repo
+	if [ $(cat /etc/system-release|grep -Po Amazon) == Amazon ]; then
+		sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+	fi
+	yum install -y apache-maven
+
+
+	echo "*********************************Installing GIT..."
+	yum install -y git
+
+}
+
 waitForAmbari () {
        	# Wait for Ambari
        	LOOPESCAPE="false"
@@ -242,6 +259,121 @@ waitForNifiServlet () {
        		sleep 2
        	done
 }
+# Import NIFI Template
+deployTemplateToNifi () {
+       	echo "*********************************Importing NIFI Template..."
+       	TEMPLATEID=$(curl -v -F template=@"$ROOT_PATH/CloudBreakArtifacts/Nifi/cdr-nifi.telco.xml" -X POST http://$AMBARI_HOST:9090/nifi-api/process-groups/root/templates/upload | grep -Po '<id>([a-z0-9-]+)' | grep -Po '>([a-z0-9-]+)' | grep -Po '([a-z0-9-]+)')
+       	sleep 1
+
+       	echo "*********************************Instantiating NIFI Flow..."
+       	# Instantiate NIFI Template
+       	curl -u admin:admin -i -H "Content-Type:application/json" -d "{\"templateId\":\"$TEMPLATEID\",\"originX\":100,\"originY\":100}" -X POST http://$AMBARI_HOST:9090/nifi-api/process-groups/root/template-instance
+       	sleep 1
+
+       	# Rename NIFI Root Group
+		echo "*********************************Renaming Nifi Root Group..."
+ROOT_GROUP_REVISION=$(curl -X GET http://$AMBARI_HOST:9090/nifi-api/process-groups/root |grep -Po '\"version\":([0-9]+)'|grep -Po '([0-9]+)')
+
+		sleep 1
+		ROOT_GROUP_ID=$(curl -X GET http://$AMBARI_HOST:9090/nifi-api/process-groups/root|grep -Po '("component":{"id":")([0-9a-zA-z\-]+)'| grep -Po '(:"[0-9a-zA-z\-]+)'| grep -Po '([0-9a-zA-z\-]+)')
+
+		PAYLOAD=$(echo "{\"id\":\"$ROOT_GROUP_ID\",\"revision\":{\"version\":$ROOT_GROUP_REVISION},\"component\":{\"id\":\"$ROOT_GROUP_ID\",\"name\":\"Biologics-Demo\"}}")
+
+		sleep 1
+		curl -d $PAYLOAD  -H "Content-Type: application/json" -X PUT http://$AMBARI_HOST:9090/nifi-api/process-groups/$ROOT_GROUP_ID
+		sleep 1
+}
+getServiceStatus () {
+       	SERVICE=$1
+       	SERVICE_STATUS=$(curl -u admin:admin -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/$SERVICE | grep '"state" :' | grep -Po '([A-Z]+)')
+
+       	echo $SERVICE_STATUS
+}
+# Start NIFI Flow
+startNifiFlow () {
+    echo "*********************************Starting NIFI Flow..."
+	# if [ "$INTVERSION" -gt 24 ]; then
+    	# Start NIFI Flow HDF 2.x
+    	TARGETS=($(curl -u admin:admin -i -X GET http://$AMBARI_HOST:9090/nifi-api/process-groups/root/processors | grep -Po '\"uri\":\"([a-z0-9-://.]+)' | grep -Po '(?!.*\")([a-z0-9-://.]+)'))
+       	length=${#TARGETS[@]}
+       	echo $length
+       	echo ${TARGETS[0]}
+
+       	for ((i = 0; i < $length; i++))
+       	do
+       		ID=$(curl -u admin:admin -i -X GET ${TARGETS[i]} |grep -Po '"id":"([a-zA-z0-9\-]+)'|grep -Po ':"([a-zA-z0-9\-]+)'|grep -Po '([a-zA-z0-9\-]+)'|head -1)
+       		REVISION=$(curl -u admin:admin -i -X GET ${TARGETS[i]} |grep -Po '\"version\":([0-9]+)'|grep -Po '([0-9]+)')
+       		TYPE=$(curl -u admin:admin -i -X GET ${TARGETS[i]} |grep -Po '"type":"([a-zA-Z0-9\-.]+)' |grep -Po ':"([a-zA-Z0-9\-.]+)' |grep -Po '([a-zA-Z0-9\-.]+)' |head -1)
+       		echo "Current Processor Path: ${TARGETS[i]}"
+       		echo "Current Processor Revision: $REVISION"
+       		echo "Current Processor ID: $ID"
+       		echo "Current Processor TYPE: $TYPE"
+
+       		if ! [ -z $(echo $TYPE|grep "PutKafka") ] || ! [ -z $(echo $TYPE|grep "PublishKafka") ]; then
+       			echo "***************************This is a PutKafka Processor"
+       			echo "***************************Updating Kafka Broker Porperty and Activating Processor..."
+       			if ! [ -z $(echo $TYPE|grep "PutKafka") ]; then
+                    PAYLOAD=$(echo "{\"id\":\"$ID\",\"revision\":{\"version\":$REVISION},\"component\":{\"id\":\"$ID\",\"config\":{\"properties\":{\"Known Brokers\":\"$AMBARI_HOST:6667\"}},\"state\":\"RUNNING\"}}")
+                else
+                    PAYLOAD=$(echo "{\"id\":\"$ID\",\"revision\":{\"version\":$REVISION},\"component\":{\"id\":\"$ID\",\"config\":{\"properties\":{\"bootstrap.servers\":\"$AMBARI_HOST:6667\"}},\"state\":\"RUNNING\"}}")
+                fi
+       		else
+       			echo "***************************Activating Processor..."
+       				PAYLOAD=$(echo "{\"id\":\"$ID\",\"revision\":{\"version\":$REVISION},\"component\":{\"id\":\"$ID\",\"state\":\"RUNNING\"}}")
+			fi
+       		echo "$PAYLOAD"
+
+       		curl -u admin:admin -i -H "Content-Type:application/json" -d "${PAYLOAD}" -X PUT ${TARGETS[i]}
+       	done
+	# else
+       	# Start NIFI Flow HDF 1.x
+		# echo "*********************************Starting NIFI Flow..."
+	#	REVISION=$(curl -u admin:admin  -i -X GET http://$AMBARI_HOST:9090/nifi-api/controller/revision |grep -Po '\"version\":([0-9]+)' | grep -Po '([0-9]+)')
+
+	#	TARGETS=($(curl -u admin:admin -i -X GET http://$AMBARI_HOST:9090/nifi-api/controller/process-groups/root/processors | grep -Po '\"uri\":\"([a-z0-9-://.]+)' | grep -Po '(?!.*\")([a-z0-9-://.]+)'))
+#length=${#TARGETS[@]}
+
+	#	for ((i = 0; i != length; i++)); do
+   #			echo curl -u admin:admin -i -X GET ${TARGETS[i]}
+   #			echo "Current Revision: " $REVISION
+   	#		curl -u admin:admin -i -H "Content-Type:application/x-www-form-urlencoded" -d "state=RUNNING&version=$REVISION" -X PUT ${TARGETS[i]}
+		#   REVISION=$(curl -u admin:admin  -i -X GET http://$AMBARI_HOST:9090/nifi-api/controller/revision |grep -Po '\"version\":([0-9]+)' | grep -Po '([0-9]+)')
+	#	done
+	# fi
+}
+
+getNameNodeHost () {
+       	NAMENODE_HOST=$(curl -u admin:admin -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/HDFS/components/NAMENODE|grep "host_name"|grep -Po ': "([a-zA-Z0-9\-_!?.]+)'|grep -Po '([a-zA-Z0-9\-_!?.]+)')
+
+       	echo $NAMENODE_HOST
+}
+
+
+getKafkaBroker () {
+       	KAFKA_BROKER=$(curl -u admin:admin -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/KAFKA/components/KAFKA_BROKER |grep "host_name"|grep -Po ': "([a-zA-Z0-9\-_!?.]+)'|grep -Po '([a-zA-Z0-9\-_!?.]+)')
+
+       	echo $KAFKA_BROKER
+}
+
+
+
+getNifiHost () {
+       	NIFI_HOST=$(curl -u admin:admin -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/NIFI/components/NIFI_MASTER|grep "host_name"|grep -Po ': "([a-zA-Z0-9\-_!?.]+)'|grep -Po '([a-zA-Z0-9\-_!?.]+)')
+
+       	echo $NIFI_HOST
+}
+
+if [ ! -d "/usr/jdk64" ]; then
+	echo "*********************************Install and Enable Oracle JDK 8"
+	wget http://public-repo-1.hortonworks.com/ARTIFACTS/jdk-8u77-linux-x64.tar.gz
+	tar -vxzf jdk-8u77-linux-x64.tar.gz -C /usr
+	mv /usr/jdk1.8.0_77 /usr/jdk64
+	alternatives --install /usr/bin/java java /usr/jdk64/bin/java 3
+	alternatives --install /usr/bin/javac javac /usr/jdk64/bin/javac 3
+	alternatives --install /usr/bin/jar jar /usr/jdk64/bin/jar 3
+	export JAVA_HOME=/usr/jdk64
+	echo "export JAVA_HOME=/usr/jdk64" >> /etc/bashrc
+fi
 
 
 instalHDFManagementPack () {
@@ -286,6 +418,7 @@ waitForServiceToStart HIVE
 
 waitForServiceToStart ZOOKEEPER
 
+waitForServiceToStart KAFKA
 sleep 10
 
 export VERSION=`hdp-select status hadoop-client | sed 's/hadoop-client - \([0-9]\.[0-9]\).*/\1/'`
@@ -304,10 +437,56 @@ sleep 2
 
 
 
+NAMENODE_HOST=$(getNameNodeHost)
+export NAMENODE_HOST=$NAMENODE_HOST
+
+ZK_HOST=$AMBARI_HOST
+export ZK_HOST=$ZK_HOST
+KAFKA_BROKER=$(getKafkaBroker)
+export KAFKA_BROKER=$KAFKA_BROKER
+# ATLAS_HOST=$(getAtlasHost)
+
+env
+
+echo "export NAMENODE_HOST=$NAMENODE_HOST" >> /etc/bashrc
+echo "export ZK_HOST=$ZK_HOST" >> /etc/bashrc
+echo "export KAFKA_BROKER=$KAFKA_BROKER" >> /etc/bashrc
+
+
+echo "export NIFI_HOST=$NIFI_HOST" >> /etc/bashrc
+
+echo "export NAMENODE_HOST=$NAMENODE_HOST" >> ~/.bash_profile
+echo "export ZK_HOST=$ZK_HOST" >> ~/.bash_profile
+echo "export KAFKA_BROKER=$KAFKA_BROKER" >> ~/.bash_profile
+
+
+echo "export NIFI_HOST=$NIFI_HOST" >> ~/.bash_profile
+
+. ~/.bash_profile
+
+echo "*********************************Installing Utlities..."
+installUtils
+
+#Configure Kafka
+ echo "*********************************Creating Kafka Topics..."
+ /usr/hdp/current/kafka-broker/bin/kafka-topics.sh --create --zookeeper $AMBARI_HOST:2181 --replication-factor 1 --partitions 1 --topic cdr
+
+
+# Build storm jar from source
+
+echo "*********************************Building CDR Storm Topology"
+cd $ROOT_PATH/telco-cdr-monitoring
+mvn clean package
+# cp -vf /root/target/telco-cdr-monitoring-1.0-SNAPSHOT.jar /home/storm
+# Deploy Storm toppology
+
+storm jar $ROOT_PATH/telco-cdr-monitoring/target/telco-cdr-monitoring-1.0-SNAPSHOT.jar com.github.gbraccialli.telco.cdr.storm.Topology $ROOT_PATH/telco-cdr-monitoring/conf/topology.props
+
+
 
 
 sleep 2
-installSchemaRegistryService
+# installSchemaRegistryService
 
 
 
@@ -328,6 +507,70 @@ if [[ $NIFI_STATUS == INSTALLED ]]; then
        	startService NIFI
 else
        	echo "*********************************NIFI Service Started..."
+fi
+waitForNifiServlet
+echo "*********************************Deploying NIFI Template..."
+deployTemplateToNifi
+
+echo "*********************************Starting NIFI Flow ..."
+startNifiFlow
+
+#Start Kafka
+KAFKA_STATUS=$(getServiceStatus KAFKA)
+echo "*********************************Checking KAFKA status..."
+if ! [[ $KAFKA_STATUS == STARTED || $KAFKA_STATUS == INSTALLED ]]; then
+       	echo "*********************************KAFKA is in a transitional state, waiting..."
+       	waitForService KAFKA
+       	echo "*********************************KAFKA has entered a ready state..."
+fi
+
+if [[ $KAFKA_STATUS == INSTALLED ]]; then
+       	startService KAFKA
+else
+       	echo "*********************************KAFKA Service Started..."
+fi
+
+#Configure Kafka
+# echo "*********************************Creating Kafka Topics..."
+# /usr/hdp/current/kafka-broker/bin/kafka-topics.sh --create --zookeeper $AMBARI_HOST:2181 --replication-factor 1 --partitions 1 --topic cdr
+
+HBASE_STATUS=$(getServiceStatus HBASE)
+echo "*********************************Checking HBASE status..."
+if ! [[ $HBASE_STATUS == STARTED || $HBASE_STATUS == INSTALLED ]]; then
+       	echo "*********************************HBASE is in a transitional state, waiting..."
+       	waitForService HBASE
+       	echo "*********************************HBASE has entered a ready state..."
+fi
+
+if [[ $HBASE_STATUS == INSTALLED ]]; then
+       	startService HBASE
+else
+       	echo "*********************************HBASE Service Started..."
+fi
+
+
+STORM_STATUS=$(getServiceStatus STORM)
+echo "*********************************Checking STORM status..."
+if ! [[ $STORM_STATUS == STARTED || $STORM_STATUS == INSTALLED ]]; then
+       	echo "*********************************STORM is in a transitional state, waiting..."
+       	waitForService STORM
+       	echo "*********************************STORM has entered a ready state..."
+fi
+
+echo "*********************************Stoping STORM Service..."
+STORM_STATUS=$(getServiceStatus STORM)
+if [[ $STORM_STATUS == STARTED ]]; then
+       	stopService STORM
+else
+       	echo "*********************************STORM Service Stopped..."
+fi
+
+echo "*********************************Starting STORM Service..."
+STORM_STATUS=$(getServiceStatus STORM)
+if [[ $STORM_STATUS == INSTALLED ]]; then
+       	startService STORM
+else
+       	echo "*********************************STORM Service Started..."
 fi
 
 
