8a302f038dc4fe10ad5ab3f301ea36d5f85ba493
geetha-hortonworks <ganne@hortonworks.com>
2017-07-14 08:29:23 -0700

    removed solr user

--- recipes/hdp-hdf-post-install.sh
+++ recipes/hdp-hdf-post-install.sh
@@ -346,20 +346,20 @@ startNifiFlow () {
 installSolr (){
 
 
-sudo -u hdfs hadoop fs -mkdir /user/solr
-sudo -u hdfs hadoop fs -chown solr /user/solr
+#sudo -u hdfs hadoop fs -mkdir /user/solr
+#sudo -u hdfs hadoop fs -chown solr /user/solr
 
 yum install lucidworks-hdpsearch
 
 cp /root/telco-cdr-monitoring/solr/* /opt/lucidworks-hdpsearch/solr/server/solr-webapp/webapp/banana/app/dashboards/
 
-chown -R solr:solr /opt/lucidworks-hdpsearch/solr
+#chown -R solr:solr /opt/lucidworks-hdpsearch/solr
 
-sudo -u solr /opt/lucidworks-hdpsearch/solr/bin/solr start -c -z  $AMBARI_HOST:2181
+ /opt/lucidworks-hdpsearch/solr/bin/solr start -c -z  $AMBARI_HOST:2181
 
 sleep 5
 
-sudo -u solr /opt/lucidworks-hdpsearch/solr/bin/solr create -c cdr \
+ /opt/lucidworks-hdpsearch/solr/bin/solr create -c cdr \
    -d data_driven_schema_configs \
    -s 1 \
    -rf 1
